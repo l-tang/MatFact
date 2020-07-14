f2cbabecb1b96e25cb1c564d44e8e32d767461af
Li Tang <litang1025@gmail.com>
2020-07-14 10:58:22 +0800

    Update mf.py

--- sui/ml/mf.py
+++ sui/ml/mf.py
@@ -18,7 +18,7 @@ __author__ = ['Li Tang']
 __copyright__ = 'Li Tang'
 __credits__ = ['Li Tang']
 __license__ = 'MIT'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __maintainer__ = ['Li Tang']
 __email__ = 'litang1025@gmail.com'
 __status__ = 'Production'
@@ -43,13 +43,25 @@ class SVDModel:
         assert matrix is not None, "'matrix' cannot be None."
         assert k > 0 and isinstance(k, int), "'k' should be an integer greater than 0."
 
-        matrix = np.array(matrix, dtype=np.float64)
-        self.matrix = (matrix - np.nanmin(matrix)) / (np.nanmax(matrix) - np.nanmin(matrix))
+        self._matrix = None
+        self._matrix_min = None
+        self._matrix_max = None
+        self.set_matrix(matrix)
         self.k = k
-        self.matrix_p = np.random.rand(len(self.matrix), self.k) if matrix_p is None else matrix_p
-        self.matrix_q = np.random.rand(self.k, len(self.matrix[0])) if matrix_q is None else matrix_q
+        self.matrix_p = np.random.rand(len(self._matrix), self.k) if matrix_p is None else matrix_p
+        self.matrix_q = np.random.rand(self.k, len(self._matrix[0])) if matrix_q is None else matrix_q
         self.name = name
         self.version = version
+
+    def set_matrix(self, matrix):
+        matrix = np.array(matrix, dtype=np.float64)
+        self._matrix_min = np.nanmin(matrix)
+        self._matrix_max = np.nanmax(matrix)
+        self._matrix = (matrix - self._matrix_min) / (self._matrix_max - self._matrix_min)
+
+    def get_matrix(self):
+        print(self._matrix)
+        return self._matrix * (self._matrix_max - self._matrix_min) + self._matrix_min
 
     def predict(self, topk=1, target='p', result_path=None):
         """
@@ -61,11 +73,11 @@ class SVDModel:
         """
         result_dict = {}
         if target == 'p':
-            for row in range(len(self.matrix)):
+            for row in range(len(self._matrix)):
                 topk_reco = []
                 score_list = []
-                for col in range(len(self.matrix[row])):
-                    if self.matrix[row, col] is None or np.isnan(self.matrix[row, col]):
+                for col in range(len(self._matrix[row])):
+                    if self._matrix[row, col] is None or np.isnan(self._matrix[row, col]):
                         score = np.matmul(self.matrix_p[row, :], self.matrix_q.T[col, :])
                         if len(topk_reco) < topk:
                             topk_reco.append(col)
@@ -78,11 +90,11 @@ class SVDModel:
                             continue
                 result_dict[row] = topk_reco
         elif target == 'q':
-            for col in range(len(self.matrix[0])):
+            for col in range(len(self._matrix[0])):
                 topk_reco = []
                 score_list = []
-                for row in range(len(self.matrix)):
-                    if self.matrix[row, col] is None or np.isnan(self.matrix[row, col]):
+                for row in range(len(self._matrix)):
+                    if self._matrix[row, col] is None or np.isnan(self._matrix[row, col]):
                         score = np.matmul(self.matrix_p[row, :], self.matrix_q.T[col, :])
                         if len(topk_reco) < topk:
                             topk_reco.append(row)
@@ -110,16 +122,16 @@ class SVDModel:
 
     def dump(self, model_file_path=None):
         """
-        
+
         :param model_file_path: Absolute path to dump this model via using pickle.
-        :return: 
+        :return:
         """
         if model_file_path is None:
             model_file_path = './{}_{}.pkl'.format(self.name, self.version)
         try:
             with open(model_file_path, 'wb') as model_output:
                 model_info = dict()
-                model_info['matrix'] = self.matrix
+                model_info['matrix'] = self.get_matrix()
                 model_info['name'] = self.name
                 model_info['version'] = self.version
                 model_info['k'] = self.k
@@ -151,7 +163,7 @@ class FunkSVD(SVDModel):
         matrix = np.array(matrix, dtype=np.float64)
         super().__init__(matrix=matrix, k=k, matrix_p=matrix_p, matrix_q=matrix_q, name=name, version=version)
 
-    def train(self, penalty='ridge', penalty_weight=0.5, learning_rate=0.75, learning_rate_decay=0.95,
+    def train(self, penalty='ridge', penalty_weight=0.5, learning_rate=0.75, learning_rate_decay=1.0,
               min_learning_rate=None, dropout=0.0, epochs=50, early_stopping=10, workers=1):
         """
         :param penalty:
@@ -188,8 +200,8 @@ class FunkSVD(SVDModel):
                     learning_rate = min_learning_rate
 
             # TODO: split matrix into multiple sections for concurrency
-            row_purview_list = [(0, len(self.matrix))]
-            col_purview_list = [(0, len(self.matrix[0]))]
+            row_purview_list = [(0, len(self._matrix))]
+            col_purview_list = [(0, len(self._matrix[0]))]
 
             # start to train
             for section_idx in range(len(row_purview_list)):
@@ -242,29 +254,29 @@ class FunkSVD(SVDModel):
 
         for row in range(row_purview[0], row_purview[1]):
             for col in range(col_purview[0], col_purview[1]):
-                if self.matrix[row, col] is None or np.isnan(self.matrix[row, col]):
+                if self._matrix[row, col] is None or np.isnan(self._matrix[row, col]):
                     continue
                 if random.random() <= 1 - dropout:
                     y_hat = np.matmul(self.matrix_p[row, :], self.matrix_q.T[col, :])
 
                     if penalty == 'ridge':
-                        self.matrix_p[row, :] += learning_rate * ((self.matrix[row, col] - y_hat) *
+                        self.matrix_p[row, :] += learning_rate * ((self._matrix[row, col] - y_hat) *
                                                                   self.matrix_q[:, col] - penalty_weight *
                                                                   self.matrix_p[row, :]) / self.k
 
-                        self.matrix_q[:, col] += learning_rate * ((self.matrix[row, col] - y_hat) *
+                        self.matrix_q[:, col] += learning_rate * ((self._matrix[row, col] - y_hat) *
                                                                   self.matrix_p[row, :] - penalty_weight *
                                                                   self.matrix_q[:, col]) / self.k
 
-                        loss += ((self.matrix[row, col] - y_hat) ** 2 + penalty_weight * (
+                        loss += ((self._matrix[row, col] - y_hat) ** 2 + penalty_weight * (
                                 np.linalg.norm(self.matrix_p[row, :]) + np.linalg.norm(
                             self.matrix_q.T[col, :]))) / self.k
                     elif penalty == 'lasso':
-                        self.matrix_p[row, :] += learning_rate * ((self.matrix[row, col] - y_hat) *
+                        self.matrix_p[row, :] += learning_rate * ((self._matrix[row, col] - y_hat) *
                                                                   self.matrix_q[:, col] - penalty_weight) / self.k
-                        self.matrix_q[:, col] += learning_rate * ((self.matrix[row, col] - y_hat) *
+                        self.matrix_q[:, col] += learning_rate * ((self._matrix[row, col] - y_hat) *
                                                                   self.matrix_p[row, :] - penalty_weight) / self.k
-                        loss += ((self.matrix[row, col] - y_hat) ** 2 + penalty_weight * (
+                        loss += ((self._matrix[row, col] - y_hat) ** 2 + penalty_weight * (
                                 np.linalg.norm(self.matrix_p[row, :], ord=1) + np.linalg.norm(
                             self.matrix_q.T[col, :], ord=1))) / self.k
                     else:
